26cd8edfa79f91f29d384b36ea63b09828ec1221
Bagdat Akhmetov <bagdatakhmetov@MacBookPro.lan>
2026-07-02 13:40:35 +0500

    style: adjust sound button layout

--- src/components/Soundboard.tsx
+++ src/components/Soundboard.tsx
@@ -12,7 +12,7 @@ export function Soundboard({ discAngle, onRecordSound, spinSpeed }: SoundboardPr
   return (
     <div className="button-sides">
       <div className="button-grid button-grid--left">
-        <ButtonColumn indexes={range(0, 10)} onRecordSound={onRecordSound} />
+        <ButtonGroup indexes={range(0, 10)} onRecordSound={onRecordSound} />
       </div>
       <div className="deck-stack">
         <NewButtonPanel indexes={range(20, 28)} onRecordSound={onRecordSound} />
@@ -20,15 +20,15 @@ export function Soundboard({ discAngle, onRecordSound, spinSpeed }: SoundboardPr
         <NewButtonPanel indexes={range(28, 36)} onRecordSound={onRecordSound} />
       </div>
       <div className="button-grid button-grid--right">
-        <ButtonColumn indexes={range(10, 20)} onRecordSound={onRecordSound} />
+        <ButtonGroup indexes={range(10, 20)} onRecordSound={onRecordSound} />
       </div>
     </div>
   );
 }
 
-function ButtonColumn({ indexes, onRecordSound }: { indexes: number[]; onRecordSound: (index: number) => void }) {
+function ButtonGroup({ indexes, onRecordSound }: { indexes: number[]; onRecordSound: (index: number) => void }) {
   return (
-    <div className="button-column">
+    <div className="button-group">
       {indexes.map((index) => (
         <SoundButton index={index} key={soundNames[index]} name={soundNames[index]} onRecordSound={onRecordSound} />
       ))}
